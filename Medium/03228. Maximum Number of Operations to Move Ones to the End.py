69e5218df95db5aed7176f4b1234ceec29587b1f
Ayush Dwivedi <91947813+AyushDwi@users.noreply.github.com>
2025-11-13 09:04:50 +0530

    Create 03228. Maximum Number of Operations to Move Ones to the End.py

---
+++ Medium/03228. Maximum Number of Operations to Move Ones to the End.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxOperations(self, s: str) -> int:
+        ops = 0
+        ones_count = 0
+        i = 0
+        
+        while i < len(s):
+            if s[i] == '1':
+                ones_count += 1
+                i += 1
+            else:
+                ops += ones_count
+                while i < len(s) and s[i] == '0':
+                    i += 1
+        
+        return ops
