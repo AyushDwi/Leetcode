ca266f9cb49191e6e541f3c068757ef15f6791ea
Ayush Dwivedi <91947813+AyushDwi@users.noreply.github.com>
2025-11-21 09:30:03 +0530

    Create 01930. Unique Length-3 Palindromic Subsequences.py

---
+++ Medium/01930. Unique Length-3 Palindromic Subsequences.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        result = 0
+        
+        for c in 'abcdefghijklmnopqrstuvwxyz':
+            left = s.find(c)
+            right = s.rfind(c)
+            
+            if right - left > 1:
+                result += len(set(s[left + 1:right]))
+        
+        return result
