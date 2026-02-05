2a0afbd4f16e6e4d438d336b9a18a80f77691e99
Ayush Dwivedi <91947813+AyushDwi@users.noreply.github.com>
2026-02-05 07:42:09 +0530

    Create 03379. Transformed Array.py

---
+++ Easy/03379. Transformed Array.py
@@ -0,0 +1,8 @@
+
+class Solution(object):
+    def constructTransformedArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return [nums[(i+nums[i])%len(nums)] for i in xrange(len(nums))]
